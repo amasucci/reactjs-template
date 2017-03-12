79b8d49293c9f2e6668234f8fa81cae56e16ae87
Antonio Masucci <eng.masucci@gmail.com>
2017-03-12 18:42:20 +0000

    Fixing style and adding eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
-/*
-    ./webpack.config.js
-*/
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})
+	template: './src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
 
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-        ]
-    },
+	entry: './src/index.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'index_bundle.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+		]
+	},
 
-    plugins: [HtmlWebpackPluginConfig]
-}
+	plugins: [HtmlWebpackPluginConfig]
+};
